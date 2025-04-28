76c3b3f40090ddbddb2fa423da27b089b43eead9
akash mali <devmali247@gmail.com>
2025-04-28 15:24:30 +0530

    dep 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { navItems } from "@/data";
-
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
@@ -11,11 +10,19 @@ import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
+import { useEffect, useState } from "react"; // ✅ add
+
 const Home = () => {
+  const [isMounted, setIsMounted] = useState(false); // ✅
+
+  useEffect(() => {
+    setIsMounted(true); // ✅ only run on client
+  }, []);
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        {isMounted && <FloatingNav navItems={navItems} />} {/* ✅ */}
         <Hero />
         <Grid />
         <RecentProjects />
